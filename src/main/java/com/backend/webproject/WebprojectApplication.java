dac685d8d483581588e2104ff528d0152e08cf90
MercadoJose <jose-eduardo.mercado@atos.net>
2022-01-14 16:37:33 -0600

    Port changes and exception added

--- src/main/java/com/backend/webproject/WebprojectApplication.java
+++ src/main/java/com/backend/webproject/WebprojectApplication.java
@@ -2,8 +2,9 @@ package com.backend.webproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WebprojectApplication {
 
 	public static void main(String[] args) {
