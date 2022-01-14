df81bdd6bd942807b51c56759918834b4df4d9d2
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-01-14 17:54:57 -0600

    (update) database connection

---
+++ src/main/java/com/backend/webproject/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.backend.webproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundo {
+
+    @RequestMapping("/")
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
+}
