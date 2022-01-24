d689da29f7eb7e1875fdcccf03a73580cf845085
AlfonsoAtos <97476797+AlfonsoAtos@users.noreply.github.com>
2022-01-23 21:46:31 -0600

    Shopping cart
    Added shopping cart jdbc, mapper, controller and bean

--- src/main/java/com/backend/webproject/entity/Payment.java
+++ src/main/java/com/backend/webproject/entity/Payment.java
@@ -18,36 +18,44 @@ public class Payment {
     @Column(name = "shoppingCartID")
     private int shoppingCartID;
 
-    
     public int getShoppingCartID() {
         return shoppingCartID;
     }
+
     public int getPaymentID() {
         return paymentID;
     }
+
     public void setPaymentID(int paymentID) {
         this.paymentID = paymentID;
     }
+
     public int getOpenCoupon() {
         return openCoupon;
     }
+
     public void setOpenCoupon(int openCoupon) {
         this.openCoupon = openCoupon;
     }
+
     public int getShoppingFinalAmount() {
         return shoppingFinalAmount;
     }
+
     public void setShoppingFinalAmount(int shoppingFinalAmount) {
         this.shoppingFinalAmount = shoppingFinalAmount;
     }
+
     public String getPaymentStatus() {
         return paymentStatus;
     }
+
     public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
     }
+
     public void setShoppingCartID(int shoppingCartID) {
         this.shoppingCartID = shoppingCartID;
     }
-    
+
 }
