6330d02983a14faed94923d655db13a71e2a0227
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-01-26 11:07:46 -0600

    Updated user controller, entity, and mapper

--- src/main/java/com/backend/webproject/jtemp/UserDAO.java
+++ src/main/java/com/backend/webproject/jtemp/UserDAO.java
@@ -3,26 +3,26 @@ package com.backend.webproject.jtemp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.backend.webproject.entity.UserData;
+import com.backend.webproject.entity.UserEntity;
+import com.backend.webproject.mapper.UserMapper;
 
 @Component
-public class JdbcTemplateRegisteredUsers {
+public class UserDAO {
 	@Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 	@Autowired
 	private UserMapper userMapper;
 	
-	public UserData getUser(String username) {
+	public UserEntity getUser(String email) {
 		Map<String, Object> params = new HashMap<>();
-		params.put("username", username);
-		List<UserData> user = jdbcTemplate.query("select * from RegisteredUser where userLoginID = :username", params, userMapper);
+		params.put("email", email);
+		List<UserEntity> user = jdbcTemplate.query("select * from RegisteredUser where userEmail = :email", params, userMapper);
 		return user.get(0);
 	}
 	
@@ -30,7 +30,7 @@ public class JdbcTemplateRegisteredUsers {
 		try {
 			String id = "(SELECT COALESCE(MAX(RegisteredUser) + 1, 1) FROM RegisteredUser)";
 			String insert = "insert into RegisteredUser values(";
-			String values = ", :email, :pass, :name, :address, :number)";
+			String values = ", :email, :pass, :name, :address, :number, 1)";
 	        String sql = insert + id + values;
 	        Map<String, Object> params = new HashMap<>();
 	        params.put("email", email);
