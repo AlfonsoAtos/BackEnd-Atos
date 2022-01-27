212473e8f5b6adcf4b2cb890ecba97a5d4c65986
MercadoJose <jose-eduardo.mercado@atos.net>
2022-01-27 11:37:56 -0600

    first test done

--- src/test/java/com/backend/webproject/CouponsTest.java
+++ src/test/java/com/backend/webproject/CouponsTest.java
@@ -1,35 +1,45 @@
-// package com.backend.webproject;
+package com.backend.webproject;
 
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
-// import java.util.List;
+import java.util.List;
 
-// import com.backend.webproject.entity.Coupons;
-// import com.backend.webproject.jtemp.JdbcTemplateCoupons;
+import com.backend.webproject.dao.CouponsDAO;
+import com.backend.webproject.entity.Coupons;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.jdbc.core.JdbcTemplate;
-// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-// class CouponsTest {
+class CouponsTest {
 
-// JdbcTemplateCoupons dao;
+    @MockBean
+    CouponsDAO couponsDAO = mock(CouponsDAO.class);;
 
-// @BeforeEach
-// void setUp() throws Exception {
-// DriverManagerDataSource dataSource = new DriverManagerDataSource();
-// dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
-// dataSource.setUsername("system");
-// dataSource.setPassword("edu511");
-// dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+    // @BeforeEach
+    // void setUp() throws Exception {
+    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+    // dataSource.setUsername("system");
+    // dataSource.setPassword("edu511");
+    // dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 
-// dao = new JdbcTemplateCoupons(new JdbcTemplate(dataSource));
-// }
+    // couponsDAO = new couponsDAO(new JdbcTemplate(dataSource));
+    // }
 
-// @Test
-// public void testList() {
-// List<Coupons> listCoupon = dao.getNewCoupons();
-// assertTrue(listCoupon.isEmpty());
-// }
-// }
+    // private CouponsDAO couponsDAO; = mock(JdbcTemplate.class);
+
+    // @BeforeEach
+    // public void beforeEach() {
+    // sequenceRepository = new SequenceRepository(jdbcTemplate);
+    // }
+
+    @Test
+    public void testList() {
+        List<Coupons> listCoupon = couponsDAO.getNewCoupons();
+        assertTrue(listCoupon.isEmpty());
+    }
+}
