33c89be22653c43969b2de3e908e9ebb3e77aaa7
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-01-27 10:47:43 -0600

    Identend correctly code

--- src/main/java/com/backend/webproject/managers/UserManager.java
+++ src/main/java/com/backend/webproject/managers/UserManager.java
@@ -13,17 +13,17 @@ public class UserManager {
 
   public User signupUser(String email, String pass, String name, String number) {
     if (db.registerUser(email, pass, name, number) == 1) {
-			// Create shopping cart
-			return db.getUser(email);
-		}
+      // Create shopping cart
+      return db.getUser(email);
+    }
     return null;
   }
 
   public User loginUser(String email, String pass) {
     User userResult = db.getUser(email);
-		if (userResult != null && userResult.getPassword() != null && userResult.getPassword().equals(pass)) {
-			return userResult;
-		}
-		return null;
+    if (userResult != null && userResult.getPassword() != null && userResult.getPassword().equals(pass)) {
+      return userResult;
+    }
+    return null;
   }
 }
