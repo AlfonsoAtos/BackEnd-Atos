a97db9531a32b2a26eb13538b9312b70319537ab
MercadoJose <jose-eduardo.mercado@atos.net>
2022-01-27 15:28:37 -0600

    Dao test and entity exclution

--- src/test/java/com/backend/webproject/CouponsTest.java
+++ src/test/java/com/backend/webproject/CouponsTest.java
@@ -8,38 +8,42 @@ import java.util.List;
 import com.backend.webproject.dao.CouponsDAO;
 import com.backend.webproject.entity.Coupons;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 class CouponsTest {
 
     @MockBean
-    CouponsDAO couponsDAO = mock(CouponsDAO.class);;
-
-    // @BeforeEach
-    // void setUp() throws Exception {
-    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    // dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
-    // dataSource.setUsername("system");
-    // dataSource.setPassword("edu511");
-    // dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-
-    // couponsDAO = new couponsDAO(new JdbcTemplate(dataSource));
-    // }
-
-    // private CouponsDAO couponsDAO; = mock(JdbcTemplate.class);
-
-    // @BeforeEach
-    // public void beforeEach() {
-    // sequenceRepository = new SequenceRepository(jdbcTemplate);
-    // }
+    CouponsDAO couponsDAO = mock(CouponsDAO.class);
 
     @Test
     public void testList() {
         List<Coupons> listCoupon = couponsDAO.getNewCoupons();
         assertTrue(listCoupon.isEmpty());
+    }
+
+    @Test
+    void testInsert() {
+        couponsDAO.insertNewCoupon(1, "Thanks Giving", "XYZ12345AB", "Open", 40, 1, 1);
+    }
+
+    @Test
+    void testUpdateCoupon() {
+        couponsDAO.updateCoupon(1, "Summer", "XYZ12345AB", "Nopen", 40, 1, 1);
+    }
+
+    @Test
+    void testDeleteCoupon() {
+        couponsDAO.deleteCoupon(1);
+    }
+
+    @Test
+    void testGetAutoCouponId() {
+        couponsDAO.getAutoCouponId();
+    }
+
+    @Test
+    void testSearchCoupon() {
+        couponsDAO.searchCouponByID(1);
     }
 }
