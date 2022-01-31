9a10322563981ba061a7b0641e1730cab529db05
AlfonsoAtos <97476797+AlfonsoAtos@users.noreply.github.com>
2022-01-31 09:37:33 -0600

    update shopping product details

--- src/main/java/com/backend/webproject/dao/ShoppingProductDetailsDAO.java
+++ src/main/java/com/backend/webproject/dao/ShoppingProductDetailsDAO.java
@@ -85,4 +85,13 @@ public class ShoppingProductDetailsDAO {
         return aux;
     }
 
+    public int finalizeShoppingProductDetail(ShoppingProductDetails spd){
+        String query = "UPDATE shoppingproductdetails SET shoppingcost=:shoppingcost, costafterapplyingcoupon=:costafterapplyingcoupon WHERE shoppingProductDetailsID=:id";
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("shoppingcost", spd.getShoppingCost());
+        params.put("costafterapplyingcoupon", spd.getCostAfterApplyingCoupon());
+        params.put("id", spd.getShoppingProductDetailsID());
+        return jdbcTemplate.update(query, params);
+    }
+
 }
