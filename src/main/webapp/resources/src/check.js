fca85cd941c4e6c56a99327cb8bbfea86f623e6e
Gómez <josue-salatiel.gomez@atos.net>
2022-01-31 21:30:21 -0600

    merge

--- src/main/webapp/resources/src/check.js
+++ src/main/webapp/resources/src/check.js
@@ -13,7 +13,13 @@ function productN(id, img, name,price,amount,sum) {
                 </svg>
             </span>
 ${amount} x ${price} = ${sum}</p>
-<button type="button" class="btn btn-secondary">Apply coupon</button>
+            <span id=${id} class="btn-plus">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+            </span>
+            <button type="button" class="btn btn-secondary">Apply coupon</button>
             </div>
             `
 }
@@ -33,7 +39,13 @@ function product0(id, img, name,price,amount,sum) {
                 </svg>
             </span>
 ${amount} x ${price} = ${sum}</p>
-<button type="button" class="btn btn-secondary">edit</button>
+            <span id=${id} class="btn-plus">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+            </span>
+            <button type="button" class="btn btn-secondary">Apply coupon</button>
             </div>
             `
 }
