10ca453ba23f4480c0ce4ff1d694b2a3cdf47498
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-02 13:27:53 -0600

    Modified PaymentManager

--- src/main/java/com/backend/webproject/managers/PaymentManager.java
+++ src/main/java/com/backend/webproject/managers/PaymentManager.java
@@ -1,28 +1,30 @@
 package com.backend.webproject.managers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.backend.webproject.dao.PaymentDAO;
 import com.backend.webproject.entity.Payment;
 
+@Component
 public class PaymentManager {
+	@Autowired
+	private PaymentDAO dao;
 
-    public int processPayment(int openCoupon, int shoppingFinalAmount, String paymentStatus, int shoppingCartID){
-        Payment payment = new Payment();
-        payment.setOpenCoupon(openCoupon);
-        payment.setShoppingFinalAmount(shoppingFinalAmount);
-        payment.setPaymentStatus(paymentStatus);
-        payment.setShoppingCartID(shoppingCartID);
-        PaymentDAO dao = new PaymentDAO();
-        return dao.processPayment(payment);
-    }
+  public int processPayment(int openCoupon, int shoppingFinalAmount, String paymentStatus, int shoppingCartID){
+    Payment payment = new Payment();
+    payment.setOpenCoupon(openCoupon);
+    payment.setShoppingFinalAmount(shoppingFinalAmount);
+    payment.setPaymentStatus(paymentStatus);
+    payment.setShoppingCartID(shoppingCartID);
+    return dao.processPayment(payment);
+  }
 
-    public Payment searchPaymentByID(int pid){
-        PaymentDAO dao = new PaymentDAO();
-        return dao.searchPaymentByID(pid);
-    }
+  public Payment searchPaymentByID(int pid){
+    return dao.searchPaymentByID(pid);
+  }
 
-    public Payment searchPaymentByCartID(int cartID){
-        PaymentDAO dao = new PaymentDAO();
-        return dao.searchPaymentByCartID(cartID);
-    }
-    
+  public Payment searchPaymentByCartID(int cartID){
+    return dao.searchPaymentByCartID(cartID);
+  }
 }
