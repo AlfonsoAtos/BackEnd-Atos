5519af1ef5181ca4cedf1446d75dc909614ab00e
AlfonsoAtos <97476797+AlfonsoAtos@users.noreply.github.com>
2022-02-02 14:25:05 -0500

    change autowired

--- src/main/java/com/backend/webproject/controller/PaymentController.java
+++ src/main/java/com/backend/webproject/controller/PaymentController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class PaymentController {
 
-  @Autowired
 	private PaymentManager manager;
     
   @RequestMapping("/byID/{id}")
