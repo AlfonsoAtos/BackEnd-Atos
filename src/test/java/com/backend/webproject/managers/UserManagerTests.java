020d57cc03410dec10130b4f23d1a8622662bf72
MercadoJose <97478439+MercadoJose@users.noreply.github.com>
2022-02-02 14:37:29 -0600

    AdminSideControllerTest and AdminSideManager(pending)

--- src/test/java/com/backend/webproject/managers/UserManagerTests.java
+++ src/test/java/com/backend/webproject/managers/UserManagerTests.java
@@ -1,66 +1,73 @@
-/*
- * package com.backend.webproject.managers;
- * 
- * import static org.junit.jupiter.api.Assertions.*;
- * 
- * import org.junit.jupiter.api.Test; import
- * org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.boot.test.context.SpringBootTest; import
- * org.springframework.boot.test.mock.mockito.MockBean;
- * 
- * import com.backend.webproject.dao.UserDAO; import
- * com.backend.webproject.entity.User;
- * 
- * import static org.mockito.BDDMockito.given;
- * 
- * @SpringBootTest class UserManagerTests {
- * 
- * @Autowired private UserManager userManager;
- * 
- * @MockBean private UserDAO userDAO;
- * 
- * @Test void loginUserTest() { User user = new User(1, "user@user.com",
- * "pass123", "User", "8110", 1);
- * 
- * given(userDAO.getUser("user@user.com")).willReturn(user);
- * 
- * User userResult = userManager.loginUser("user@user.com", "pass123");
- * assertEquals(user, userResult); }
- * 
- * @Test void loginUserWrongInfoTest() {
- * given(userDAO.getUser("user@user.com")).willReturn(null);
- * 
- * User userResult = userManager.loginUser("user@user.com", "pass123");
- * assertEquals(null, userResult);
- * 
- * User user = new User(1, "user2@user.com", "pass", "User", "8110", 1);
- * 
- * given(userDAO.getUser("user2@user.com")).willReturn(user); userResult =
- * userManager.loginUser("user2@user.com", "pass123"); assertEquals(null,
- * userResult); }
- * 
- * @Test void loginUserNullValuesTest() { assertEquals(null,
- * userManager.loginUser("user@user.com", null)); assertEquals(null,
- * userManager.loginUser(null, "pass123")); assertEquals(null,
- * userManager.loginUser(null, null)); }
- * 
- * 
- * @Test void signupUserTest() { User user = new User(1, "user@user.com",
- * "pass123", "User", "8110", 1);
- * 
- * given(userDAO.registerUser("user@user.com", "pass123", "User",
- * "8110")).willReturn(1);
- * given(userDAO.getUser("user@user.com")).willReturn(user);
- * 
- * User userResult = userManager.signupUser("user@user.com", "pass123", "User",
- * "8110"); assertEquals(user, userResult); }
- * 
- * @Test void signupUserErrorTest() {
- * given(userDAO.registerUser("user@user.com", "pass123", "User",
- * "8110")).willReturn(0);
- * 
- * User userResult = userManager.signupUser("user@user.com", "pass123", "User",
- * "8110"); assertEquals(null, userResult); }
- * 
- * }
- */
+package com.backend.webproject.managers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.backend.webproject.dao.UserDAO;
+import com.backend.webproject.entity.User;
+
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+class UserManagerTests {
+    @Autowired
+    private UserManager userManager;
+
+    @MockBean
+    private UserDAO userDAO;
+
+    @Test
+    void loginUserTest() {
+        User user = new User(1, "user@user.com", "pass123", "User", "8110", 1);
+
+        given(userDAO.getUser("user@user.com")).willReturn(user);
+
+        User userResult = userManager.loginUser("user@user.com", "pass123");
+        assertEquals(user, userResult);
+    }
+
+    @Test
+    void loginUserWrongInfoTest() {
+        given(userDAO.getUser("user@user.com")).willReturn(null);
+
+        User userResult = userManager.loginUser("user@user.com", "pass123");
+        assertEquals(null, userResult);
+
+        User user = new User(1, "user2@user.com", "pass", "User", "8110", 1);
+
+        given(userDAO.getUser("user2@user.com")).willReturn(user);
+        userResult = userManager.loginUser("user2@user.com", "pass123");
+        assertEquals(null, userResult);
+    }
+
+    @Test
+    void loginUserNullValuesTest() {
+        assertEquals(null, userManager.loginUser("user@user.com", null));
+        assertEquals(null, userManager.loginUser(null, "pass123"));
+        assertEquals(null, userManager.loginUser(null, null));
+    }
+
+    @Test
+    void signupUserTest() {
+        User user = new User(1, "user@user.com", "pass123", "User", "8110", 1);
+
+        given(userDAO.registerUser("user@user.com", "pass123", "User", "8110")).willReturn(1);
+        given(userDAO.getUser("user@user.com")).willReturn(user);
+
+        User userResult = userManager.signupUser("user@user.com", "pass123", "User", "8110");
+        assertEquals(user, userResult);
+    }
+
+    @Test
+    void signupUserErrorTest() {
+        given(userDAO.registerUser("user@user.com", "pass123", "User", "8110")).willReturn(0);
+
+        User userResult = userManager.signupUser("user@user.com", "pass123", "User", "8110");
+        assertEquals(null, userResult);
+    }
+
+}
